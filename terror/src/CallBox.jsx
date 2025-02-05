1055f8c8e02ecb4934b043d98a10cf4db5fd034a
seanpatrickmoran <seanpatrickmoran@gmail.com>
2025-02-05 13:30:50 -0500

    resize added

--- terror/src/CallBox.jsx
+++ terror/src/CallBox.jsx
@@ -1,4 +1,7 @@
-import Draggable from 'react-draggable';
+// import Draggable from 'react-draggable';
+// import { Resizable } from 'react-resizable';
+import { Rnd } from "react-rnd";
+
 // import React, {useState} from 'react';
 import React, {useEffect,useState} from 'react';
 import ollama from 'ollama';
@@ -7,68 +10,75 @@ import "./CallBox.css"
 
 
 export class CallBox extends React.Component{
-	 constructor(props){
-	    super(props);
-	this.closeWindow = this.closeWindow.bind(this);
-	    this.state = {
-	    	term: "$>",
-	    	written: "",
-	    	messages: [],
-	    	};
-	   }
+	constructor(props){
+		super(props);
+		this.closeWindow = this.closeWindow.bind(this);
+    this.state = {
+    	term: "$>",
+    	written: "",
+    	messages: [],
+	    width: 640,
+	    height: 480,
+	    x: 0,
+	    y: 0,
+		};
+	}
 
-  componentDidMount() {
-				// this.startUpLlama(`Hey. You always will respond like you're my impatient, smart, kind of mean big sister. I'm late. Play along. Do not speak about this message. Do not refer to yourself as a Large Language Model.`);
-				this.startUpLlama(`Hello!`);
-	  }
+	componentDidMount() {
+		// this.startUpLlama(`Hey. You always will respond like you're my impatient, smart, kind of mean big sister. I'm late. Play along. Do not speak about this message. Do not refer to yourself as a Large Language Model.`);
+		this.startUpLlama(`Hello!`);
+	}
 
 
-    startUpLlama = async (message) => {
+	startUpLlama = async (message) => {
+		const response = await ollama.chat({
+		  model: 'llama3.2',
+		  messages: [{role: 'user', content: `${message}`}],
+		})
 
-			const response = await ollama.chat({
-			  model: 'llama3.2',
-			  messages: [{role: 'user', content: `${message}`}],
-			})
-
-			const responseWritten = "@>"+ `${response.message.content}` +"\n" + "\n";
-			var holdMessages = this.state.messages;
-			holdMessages.push({role: 'system', content: `${response.message.content}`})
-			this.setState({
-				term: "$>",
-				written: responseWritten,
-				messages: holdMessages,
-			})
-			this.scrollToBottom();
+		const responseWritten = "@>"+ `${response.message.content}` +"\n" + "\n";
+		var holdMessages = this.state.messages;
+		holdMessages.push({role: 'system', content: `${response.message.content}`})
+		this.setState({
+			term: "$>",
+			written: responseWritten,
+			messages: holdMessages,
+		})
+		this.scrollToBottom();
    };
 
-   llamaSpeak = async () => {
-				const newWritten = this.state.written + `${this.state.term}` + "\n";
+	llamaSpeak = async () => {
+		const newWritten = this.state.written + `${this.state.term}` + "\n";
 
-				var holdMessages = this.state.messages
-				holdMessages.push({role: 'user', content: `${this.state.term}` });
-				this.setState({messages: holdMessages})
+		var holdMessages = this.state.messages
+		holdMessages.push({role: 'user', content: `${this.state.term}` });
+		this.setState({messages: holdMessages})
 
-				const response = await ollama.chat({
-				  model: 'llama3.2',
-				  messages: this.state.messages,
-				})
+		const response = await ollama.chat({
+		  model: 'llama3.2',
+		  messages: this.state.messages,
+		})
 
-				const responseWritten = this.state.written + `${this.state.term}` + "\n" + "\n" + "@>"+ `${response.message.content}` +"\n" + "\n";
-				holdMessages.push({role: 'system', content: `${response.message.content}`})
-				this.setState({
-					term: "$>",
-					written: responseWritten,
-					messages: holdMessages,
-				})
-				this.scrollToBottom();
+		const responseWritten = this.state.written + `${this.state.term}` + "\n" + "\n" + "@>"+ `${response.message.content}` +"\n" + "\n";
+		holdMessages.push({role: 'system', content: `${response.message.content}`})
+		this.setState({
+			term: "$>",
+			written: responseWritten,
+			messages: holdMessages,
+		})
+		this.scrollToBottom();
    }
 
 
-  onKeyPressHandler = async (e) => {
-     if (e.key === 'Enter') {
-     	this.llamaSpeak();
-     }
+	onKeyPressHandler = async (e) => {
+		if (e.key === 'Enter') {
+			this.llamaSpeak();
+		}
  };
+
+   onResize = (event, {node, size, handle}) => {
+    this.setState({width: size.width, height: size.height});
+  };
 
   scrollToBottom() {
   	let scrollableDiv = document.getElementById('talk');
@@ -85,27 +95,37 @@ export class CallBox extends React.Component{
 
   return <>
 
-				<Draggable
-				handle="#callBoxTitle"
-				position={null}
-				scale={1}
-				onStart={this.handleStart}
-				onDrag={this.handleDrag}
-				onStop={this.handleStop}>
 
-			  <div id="callBox" className="content" style={{visibility: this.props.isCalling}}>
-			    <div id="callBoxTitle" className="headerTitle">
-			      <div className="titleLines"></div>
-			      <div className="titleLines"></div>
-			      <div className="titleLines"></div>
-			      <div className="titleLines"></div>
-			      <div className="titleLines"></div>
-			      <div className="titleLines"></div>
-			      <div id="callBoxTitleHandle" className="callTitle">CALL</div>
-			      <div id="callBoxTitleCloseBox" className="control-box close-box" onClick={this.closeWindow} >
-			      <a id="callBoxTitleCloseInner" className="control-box-inner"></a>
-			      </div>
-			    </div>
+
+		<Rnd
+			className="content"
+			dragHandleClassName="headerTitle"
+			minWidth={340}
+			minHeight={200}
+		  size={{ width: this.state.width,  height: this.state.height }}
+		  position={{ x: this.state.x, y: this.state.y }}
+		  onDragStop={(e, d) => { this.setState({ x: d.x, y: d.y }) }}
+		  onResizeStop={(e, direction, ref, delta, position) => {
+		    this.setState({
+		      width: ref.style.width,
+		      height: ref.style.height,
+		      ...position,
+		    });
+		  }}
+		>
+	  {/*<div id="callBox" className="content" style={{visibility: this.props.isCalling, width: this.state.width + 'px', height: this.state.height + 'px'}}>*/}
+	    <div id="callBoxTitle" className="headerTitle">
+	      <div className="titleLines"></div>
+	      <div className="titleLines"></div>
+	      <div className="titleLines"></div>
+	      <div className="titleLines"></div>
+	      <div className="titleLines"></div>
+	      <div className="titleLines"></div>
+	      <div id="callBoxTitleHandle" className="callTitle">CALL</div>
+	      <div id="callBoxTitleCloseBox" className="control-box close-box" onClick={this.closeWindow} >
+	      <a id="callBoxTitleCloseInner" className="control-box-inner"></a>
+	      </div>
+	    </div>
 	    <div id="talk" style={{color:"#666"}}>
 	    	<pre>{this.state.written}</pre>
 	    </div>
@@ -117,19 +137,13 @@ export class CallBox extends React.Component{
 	    		onChange={e=>this.setState({term: e.target.value})}
           autoComplete="off"
           onKeyPress={this.onKeyPressHandler}
-          autoFocus="autofocus"
+          autoFocus="autofocus"/>
 
-	    	/>
-	    </div>
-
-{/*	    <div className="row-container">
-	      <button type="button" id="intersectBtn">SEND</button>
-	      <button type="button" id="aNotbBtn">RECV</button>
-	      <button type="button" id="bNotaBtn">CLEAR</button>
-	    </div>*/}
+    {/*</div>*/}
 	  </div>
+    {/*</div>*/}
 
-	  </Draggable>
+	  </Rnd>
 	  </>
   }
 }
